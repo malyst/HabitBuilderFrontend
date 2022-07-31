83814ea6b8b0def2b4fefb4d1958622a986716e2
Daisuke Hashimoto <daisukehashimoto39@gmail.com>
2022-07-31 13:13:27 -0400

    Reorganized the project structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { ModalComponent } from './components/modal/modal.component';
-import { IndexComponent } from './components/index/index.component';
-import { HomeHabitListComponent } from './components/home-habit-list/home-habit-list.component';
+import { RegisterComponent } from './components/home/modal/register/register.component';
+import { LoginComponent } from './components/home/modal/login/login.component';
+import { ModalComponent } from './components/home/modal/modal.component';
+import { IndexComponent } from './components/home/modal/index/index.component';
+import { HomeHabitListComponent } from './components/home/home-habit-list/home-habit-list.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 import { APIConnecterService } from './services/apiconnecter.service';
